1cc548291710c7cc43475a0995243f041e4728b6
leepolla <leepolla@gmail.com>
2018-02-28 21:56:21 -0800

    added some basic variables for keeping track of hands and displaying cards

--- blackjack.js
+++ blackjack.js
@@ -11,3 +11,37 @@ function shuffle(decks){
 }
 shuffle(1);
 console.log(deck);
+
+var dealer = [];
+var player1 = [];
+var turn = 'player1';
+var deckSize = 1;
+
+function draw(hand){
+    
+    if (deck.length < 11){
+        shuffle(deckSize) //adjust to global  deck size set by user
+    }
+    card = deck[Math.floor(Math.random() * deck.length)]
+    
+    deck.splice(deck.indexOf(card), 1);
+    hand.push(card);
+    update();
+}
+
+var svg = d3.select("#cardArea");
+svg.attr("width", 700)
+    .attr("height", 500);
+function update(){
+var p1Hand = svg.selectAll("rect")
+    .data(player1)
+    .enter()
+    .append('rect')
+    .attr("x", 10)
+    .attr("y", 10)
+    .attr("width", 70)
+    .attr("height", 150)
+    .attr("class", "card")
+    .attr("text", function(d){return d;});
+}
+player1.push(draw(player1))
