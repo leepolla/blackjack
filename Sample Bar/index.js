81c9963dcab2a85c491ace01c5963e7f1e9eb70d
Jacques Wyatt Therrien <jake.therrien13@gmail.com>
2018-03-04 18:00:22 -0800

    one visual working

--- Sample Bar/index.js
+++ Sample Bar/index.js
@@ -4,10 +4,54 @@ var margin = {top: 20, right: 15, bottom: 30, left: 40};
 var w = width - margin.left - margin.right;
 var h = height - margin.top - margin.bottom;
 
-var tooltip = d3.select("body").select("#visualization").append("div")
-.attr("class", "tooltip")
-.style("opacity", 1)
-.style("position", "absolute");
+var deck = [];
+var counts = [];
+var dealer = [];
+var values = {'a':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'j':10,'q':10,'k':10};
+var countValues = {'a':-1,'2':1,'3':1,'4':1,'5':1,'6':1,'7':0,'8':0,'9':0,'10':-1,'j':-1,'q':-1,'k':-1};
+var player1 = [];
+var discard = [];
+var cardCounts = [{'name':'-1', 'value':0}, {'name': '0', 'value':0}, {'name':'1', 'value':0}]
+
+
+function shuffle(decks){
+  deck = [];
+  for (i=0; i<(decks*4);i++){
+      Object.keys(values).forEach(function(card){
+          deck.push(card);
+      });
+  }
+}
+shuffle(1);
+console.log(deck);
+
+function drawPlayer(index){
+  card = deck[index] 
+  deck.splice(deck.indexOf(card), 1);
+  discard.push(card);
+  player1.push(card)
+}
+drawPlayer(2)
+drawPlayer(2)
+
+function drawDealer(index){
+  card = deck[index] 
+  deck.splice(deck.indexOf(card), 1);
+  discard.push(card);
+  dealer.push(card)
+}
+drawDealer(6)
+
+
+function getCount(){
+  discard.forEach(function(card) {
+    val = countValues[card];
+    val = parseInt(val);
+    cardCounts[val+1].value++;
+  })
+}
+getCount()
+
 
 var x = d3.scaleBand()
         .range([0,w])
@@ -16,8 +60,6 @@ var x = d3.scaleBand()
 var y = d3.scaleLinear()
         .range([h, 0]);
 
-var colorScale = d3.scaleQuantile()
-    .range(["#CD7F32", "#C0C0C0", "#FFD700"]);
 
 var svg = d3.select("body").append("svg")
 .attr("width", w + margin.left + margin.right)
@@ -25,20 +67,8 @@ var svg = d3.select("body").append("svg")
 .append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var dataset;
 
-d3.csv("winter_medals2.csv", function(error, winter) {
-  //read in the data
-  if (error) throw error;
 
-  winter.forEach(function(d) {
-    d.Year = +d.Year;
-    d.MedalCount = +d.MedalCount;
-  });
-
-    dataset = winter;
-    drawVis(dataset, 1994);
-});
 
 svg.append("g")
     .attr("class", "x axis")
@@ -46,54 +76,23 @@ svg.append("g")
 svg.append("g")
     .attr("class","y axis");
 
-function drawVis(dataset, year) {
-  dataset = dataset.filter(function(d){return d.Year==year;});
-  console.log(dataset)
 
-   x.domain(dataset.map(function(d) {return d.Country;}));
-   y.domain([0, d3.max(dataset, function(d){return d.MedalCount;})]);
-   colorScale.domain([0, d3.max(dataset, function(d){return d.MedalCount;})]);
+counts = [cardCounts[0].value, cardCounts[1].value, cardCounts[2].value]
+names = [cardCounts[0].name, cardCounts[1].name, cardCounts[2].name]
+
+function drawCounts() {
+   x.domain(cardCounts.map(function(d) {return d.name}));
+   y.domain([0, d3.max(counts)]);
 
   var bars = svg.selectAll(".bar")
-    .data(dataset)
-
-  bars
-    .exit()
-    .remove();
-  var new_bars = bars
+    .data(cardCounts)
     .enter()
     .append("rect")
     .attr("class", "bar")
-    .attr("x", function(d) {return x(d.Country); })
+    .attr("x", function(d) {return x(d.name); })
     .attr("width", function(d) {return x.bandwidth();})
-    .attr("y", h)
-    .attr("height", 0)
-    .on("mouseover", function(d){
-      tooltip.style("opacity", 0.9);
-      tooltip.html("Country: " + d.Country + "<br/>Medals: " + d.MedalCount)
-      .style("left", (d3.event.pageX) + "px")
-      .style("top", ((d3.event.pageY) - 28) + "px");
-      })
-    .on("mouseout", function(d) {
-      tooltip.style("opacity", 0);
-    })
-
-    new_bars.merge(bars)
-      .transition(1000)
-    .attr("x", function(d) {return x(d.Country); })
-    .attr("width", function(d) {
-      return x.bandwidth();
-    })
-    .attr("fill", function(d) {
-        return colorScale(d.MedalCount);
-      })
-     .attr("y", function(d) { 
-        return y(d.MedalCount);
-      })
-      .attr("height", function(d) { return (h - y(d.MedalCount)); 
-      });
-      
-
+    .attr("y", function(d) {return y(d.value)})
+    .attr("height", function(d) {return h - y(d.value)})      
 
   svg.select(".x.axis")
     .transition(1000)
@@ -102,11 +101,44 @@ function drawVis(dataset, year) {
     .transition(1000)
     .call(d3.axisLeft(y));
 }
+drawCounts();
 
-var slider = d3.select("#olympic");
-slider.on("change", function() {
-  console.log(this.value);
-  drawVis(dataset, this.value);
-});
+function playerCount() {
+  var total = 0;
+  player1.forEach(function(d) {
+    total += values[d];
+  });
+  if (player1.includes('a') && total <= 11) {
+    total += 10;
+  }
+  console.log(total);
+  return total;
+}
+
+playerCount();
+
+function drawProbs() {
+  x.domain(cardCounts.map(function(d) {return d.name}));
+  y.domain([0, d3.max(counts)]);
+
+ var bars = svg.selectAll(".bar")
+   .data(cardCounts)
+   .enter()
+   .append("rect")
+   .attr("class", "bar")
+   .attr("x", function(d) {return x(d.name); })
+   .attr("width", function(d) {return x.bandwidth();})
+   .attr("y", function(d) {return y(d.value)})
+   .attr("height", function(d) {return h - y(d.value)})      
+
+ svg.select(".x.axis")
+   .transition(1000)
+   .call(d3.axisBottom(x));
+ svg.select(".y.axis")
+   .transition(1000)
+   .call(d3.axisLeft(y));
+}
+drawProbs();
+
 
  
